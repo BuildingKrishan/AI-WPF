512ebbf53cb7b7e395de60cadccc0152bad49d33
KRISHAN BANSAL <kribansal@microsoft.com>
2024-02-18 23:10:07 +0530

    Added my project

---
+++ AI-WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Windows.Vision;
+using Microsoft.Windows.Imaging;
+
+namespace AI_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Text_Button_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(new Uri("TextView.xaml", UriKind.Relative));
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            //frame.Navigate(new Uri("TextView.xaml", UriKind.Relative));
+            var textRecognizer = new Microsoft.Windows.Imaging.MagicEraser();
+
+        }
+    }
+}
